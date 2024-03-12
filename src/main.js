798fc990b2378b38e9954315dc1a73e9af0d9733
unknown <samuel.linkon@rlg.com.br>
2024-03-11 22:37:32 -0300

    feature: Login Page

--- src/main.js
+++ src/main.js
@@ -3,4 +3,15 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+// Vuetify
+import 'vuetify/styles/main.css'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/lib/components'
+import * as directives from 'vuetify/lib/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+createApp(App).use(store).use(router).use(vuetify).mount("#app");
---
+++ src/views/Login.vue
@@ -0,0 +1,60 @@
+<template>
+    <v-form>
+      <v-container>
+        <v-row class="login">
+          <v-col
+            cols="12"
+            md="2"
+          >
+            <v-text-field
+              v-model="name"
+              :counter="10"
+              :rules="nameRules"
+              label="Nome"
+              required
+              type="email"
+            >
+        </v-text-field>
+        <v-text-field
+            v-model="paswword"
+            :counter="10"
+            :rules="nameRules"
+            label="Senha"
+            type="password"
+            required
+          ></v-text-field>
+          <v-btn @click="onClick">
+            Entrar
+          </v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
+  </template>
+  
+  <script>
+  export default {
+    data(){
+        return{
+            name:'',
+            password:''
+        }
+    },
+    methods: {
+      onClick() {
+        alert('Botão clicado!');
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .v-row.login {
+    /* gap: 10px; */
+    justify-content: center;
+}
+.v-row.login .v-input {
+    margin: 15px 0px;
+}
+</style>
+  
